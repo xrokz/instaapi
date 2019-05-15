d5e163f05035cbfebb3c9d960e9f459d78d8a1ca
xRokz <38587835+rokzartz@users.noreply.github.com>
2019-05-15 19:50:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const fetch = require("node-fetch");
 
-async function search(name) {
-	if (!name) throw new Error('TypeError: you must previod a username');
-	const req = await fetch(`https://www.instagram.com/web/search/topsearch/?context=user&count=0&query=${encodeURIComponent(name)}`);
-	const res = await req.json();
-	
-	return res.users;
+class Instagram {
+	constructor(username, pass) {
+		this.user = username;
+		this.pass = pass;
+	}
 }
 
-
 module.exports = {
-    search
+    Instagram
 }
